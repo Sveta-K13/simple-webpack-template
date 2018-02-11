4728954a9ceebda276a67ad4b87ffc5b47f3f947
Svetlana Krivosheeva <ly4chik.sveta@gmail.com>
2018-02-12 00:03:10 +0600

    step for multilang

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const I18nPlugin = require("i18n-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const languages = {
   "en": null,
   "de": require("./languages/de.json")
@@ -8,12 +9,13 @@ const languages = {
 
 const indexHtml = path.join(__dirname, "src", "index.html");
 
+
+
 module.exports = Object.keys(languages).map(function(language) {
   return {
     name: language,
     entry: [
         path.join(__dirname, "src", "index.js"),
-        indexHtml
     ],
     output: {
       path: path.resolve(__dirname, 'dist'),
@@ -21,55 +23,71 @@ module.exports = Object.keys(languages).map(function(language) {
       publicPath: '/dist',                          // New
     },
     module: {
-        rules: [
-            {
-                test: indexHtml,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            name: "[name].[ext]",
-                        },
-                    },
-                    {
-                        loader: "extract-loader",
-                    },
-                    {
-                        loader: "html-loader",
-                        options: {
-                            attrs: ["img:src", "link:href"],
-                            interpolate: true,
-                            minimize: true,
-                            root: path.resolve(__dirname, 'assets'),
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.css$/,
-                loaders: [
-                    {
-                        loader: "file-loader",
-                    },
-                    {
-                        loader: "extract-loader",
-                    },
-                ],
-            },
-            {
-                test: /\.jpg$/,
-                loaders: [
-                    {
-                        loader: "file-loader"
-                    },
-                ],
-            },
-        ]
+      rules: [
+          // {
+          //     test: indexHtml,
+          //     use: [
+          //         {
+          //             loader: "file-loader",
+          //             options: {
+          //                 name: "[name]." + language + ".[ext]",
+          //             },
+          //         },
+          //         {
+          //             loader: "extract-loader",
+          //         },
+          //         {
+          //             loader: "html-loader",
+          //             options: {
+          //                 attrs: ["img:src", "link:href"],
+          //                 interpolate: true,
+          //                 minimize: true,
+          //                 root: path.resolve(__dirname, 'assets'),
+          //             },
+          //         },
+          //     ],
+          // },
+          {
+              test: /\.css$/,
+              loaders: [
+                  {
+                      loader: "file-loader",
+                  },
+                  {
+                      loader: "extract-loader",
+                  },
+              ],
+          },
+          {
+              test: /\.jpg$/,
+              loaders: [
+                  {
+                      loader: "file-loader"
+                  },
+              ],
+          },
+      ],
+      loaders: [
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        options: {
+            attrs: ["img:src", "link:href"],
+            interpolate: true,
+            minimize: true,
+            root: path.resolve(__dirname, 'dist'),
+        },
+      },
+      ],
     },
     plugins: [
       new I18nPlugin(
-        languages[language]
-      )
+        languages[language],
+      ),
+       new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        filename: 'index.' + language + '.html',
+      }),
     ]
   };
 });
